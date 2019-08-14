d2876fa65aae3886e10d5fb04fffc65e267047f4
Esslinger <Simon.Esslinger@homag.com>
2019-08-14 16:24:25 +0800

    Add build powershell script

---
+++ build.ps1
@@ -0,0 +1,18 @@
+if (-not (Get-Command biber -errorAction SilentlyContinue)){
+    Write-Error 'biber is not installed. Aborting."' -ErrorAction Stop
+}
+
+if (-not (Get-Command pdflatex -errorAction SilentlyContinue)){
+    Write-Error 'pdflatex is not installed. Aborting."' -ErrorAction Stop
+}
+
+if (Get-Command latexmk -errorAction SilentlyContinue)
+{
+    Write-Host "Using latexmk"
+    latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf root.tex
+} else {
+    pdflatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
+    biber root
+    pdflatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
+    pdflatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
+}
