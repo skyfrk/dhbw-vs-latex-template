c7f59e149fa73bd2dbd4ace833b9c52d1d20213e
Simon <27026029+skyfrk@users.noreply.github.com>
2019-08-14 21:58:33 +0800

    Fix quotation marks

--- build.ps1
+++ build.ps1
@@ -1,14 +1,14 @@
 if (-not (Get-Command biber -errorAction SilentlyContinue)){
-    Write-Error 'biber is not installed. Aborting."' -ErrorAction Stop
+    Write-Error 'biber is not installed. Aborting.' -ErrorAction Stop
 }
 
 if (-not (Get-Command pdflatex -errorAction SilentlyContinue)){
-    Write-Error 'pdflatex is not installed. Aborting."' -ErrorAction Stop
+    Write-Error 'pdflatex is not installed. Aborting.' -ErrorAction Stop
 }
 
 if (Get-Command latexmk -errorAction SilentlyContinue)
 {
-    Write-Host "Using latexmk"
+    Write-Host 'Using latexmk'
     latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf root.tex
 } else {
     pdflatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
