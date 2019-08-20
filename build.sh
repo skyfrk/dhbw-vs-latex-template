ef2740b4290c9ed274930cfe1cc3d9c2c28c9674
Esslinger <Simon.Esslinger@homag.com>
2019-08-20 11:01:52 +0800

    Remove hardcoded root.tex

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+FILE=$1
+FILE_WITHOUT_EXT=${FILE%.*}
+
 if ! [ -x "$(command -v lualatex)" ]; then
   echo 'lualatex is not installed. Aborting.' >&2
   exit 1
@@ -12,13 +15,13 @@ fi
 
 if [ -x "$(command -v latexmk)" ]; then
   echo 'Using latexmk to compile.' >&2
-  latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -pdflatex=lualatex root.tex
+  latexmk -synctex=1 -interaction=nonstopmode -file-line-error -pdf -pdflatex=lualatex $FILE
   exit 0
 fi
 
-lualatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
-biber root
-lualatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
-lualatex -synctex=1 -interaction=nonstopmode -file-line-error root.tex
+lualatex -synctex=1 -interaction=nonstopmode -file-line-error $FILE
+biber $FILE_WITHOUT_EXT
+lualatex -synctex=1 -interaction=nonstopmode -file-line-error $FILE
+lualatex -synctex=1 -interaction=nonstopmode -file-line-error $FILE
 
 exit 0
