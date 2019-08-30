76b4039f1094e8de808d50d1ba977e9cbd004af9
skyfrk <si-mon@posteo.de>
2019-08-30 16:50:14 +0800

    Update install scripts to only copy prefixed files

--- install_udhbwvst_tl.ps1
+++ install_udhbwvst_tl.ps1
@@ -2,6 +2,6 @@ $path = $(kpsewhich -var-value TEXMFHOME)
 
 New-Item -ItemType Directory -Force -Path $path/tex/latex/udhbwvst
 
-Copy-Item -Recurse -force ./src/* $path/tex/latex/udhbwvst
+Copy-Item -Recurse -force ./src/udhbwvst* $path/tex/latex/udhbwvst
 
 texhash $path
